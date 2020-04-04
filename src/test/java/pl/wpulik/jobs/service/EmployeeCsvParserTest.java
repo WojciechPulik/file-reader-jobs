a03a42f30e16580a6cf39a8e134f853eeffd1db7
WojciechPulik <59091416+WojciechPulik@users.noreply.github.com>
2020-04-04 17:51:35 +0200

    Update EmployeeCsvParserTest.java
    new test added

--- src/test/java/pl/wpulik/jobs/service/EmployeeCsvParserTest.java
+++ src/test/java/pl/wpulik/jobs/service/EmployeeCsvParserTest.java
@@ -3,6 +3,9 @@ package pl.wpulik.jobs.service;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +17,12 @@ public class EmployeeCsvParserTest {
 	EmployeeCsvParser csvParser;
 	
 	@Test
+	public void csvReaderTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, 
+	IllegalArgumentException, InvocationTargetException {
+		assertNotNull(invokePrivateMethodcsvReader());
+	}
+	
+	@Test
 	public void GetEmployeeDataTest() {
 		assertNotNull(csvParser.getEmployeeData());
 	}
@@ -21,6 +30,15 @@ public class EmployeeCsvParserTest {
 	@Test
 	public void GetEmployeeIdTest() {
 		assertFalse(csvParser.getEmployeeData().get(0).getId() == 0);
+	}
+	
+	
+	private String invokePrivateMethodcsvReader() throws IllegalAccessException, IllegalArgumentException, 
+	InvocationTargetException, NoSuchMethodException, SecurityException {
+		Class<?> clazz = csvParser.getClass();
+		Method method = clazz.getDeclaredMethod("csvReader");
+		method.setAccessible(true);
+		return (String) method.invoke(csvParser);
 	}
 
 }
